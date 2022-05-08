a0cef61f550208bb84ef138b0b30f87fa155eb28
alstn <63348675+termsigening97@users.noreply.github.com>
2022-05-08 12:13:36 +0900

    gfdgf

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,9 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+  RouteRecordRaw,
+} from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
@@ -19,7 +24,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+/* eslint-disable prettier/prettier */
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+    transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === "production" ? "/baemin_proj/" : "/",
 });
