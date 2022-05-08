1bdec63b6b1cc68d485ea5fbb1d68572b7927623
alstn <63348675+termsigening97@users.noreply.github.com>
2022-05-08 11:29:38 +0900

    main.yml

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,13 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import BootstrapVue3 from "bootstrap-vue-3";
+
+// Optional, since every component import their Bootstrap functionality
+// the following line is not necessary
+// import 'bootstrap'
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+
+createApp(App).use(router).use(BootstrapVue3).mount("#app");
