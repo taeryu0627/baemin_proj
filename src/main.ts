892e8537aa656e8cfffcbe2bb9d49804c9ace6c2
alstn <63348675+termsigening97@users.noreply.github.com>
2022-05-15 10:52:03 +0900

    asdfdasf

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,24 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
 createApp(App).use(router).use(BootstrapVue3).mount("#app");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZvawo4JnISANMiPaihzYHXpaw_8qy0VU",
+  authDomain: "baemin-taegeun.firebaseapp.com",
+  projectId: "baemin-taegeun",
+  storageBucket: "baemin-taegeun.appspot.com",
+  messagingSenderId: "52084758333",
+  appId: "1:52084758333:web:c09c2f4fdb2d04a3cb5754",
+  measurementId: "G-7F93V4EPEM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
