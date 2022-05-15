081bcd4b59010f07ff00e83d74faaec3c98cc9e2
taeryu0627 <taeryu0627@users.noreply.github.com>
2022-05-15 02:08:34 +0000

    deploy: b663f8d2bc7d99806bae1d47faac7a17fa021058

---
+++ js/about.52c35773.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkbaemin_proj"]=self["webpackChunkbaemin_proj"]||[]).push([[443],{4335:function(n,a,e){e.r(a),e.d(a,{default:function(){return f}});var r=e(3396);const u={class:"about"},t=(0,r._)("h1",null,"This is an about page",-1),s=[t];function i(n,a){return(0,r.wg)(),(0,r.iD)("div",u,s)}var c=e(89);const o={},b=(0,c.Z)(o,[["render",i]]);var f=b}}]);
+//# sourceMappingURL=about.52c35773.js.map
